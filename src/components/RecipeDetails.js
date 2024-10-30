fd2ef7e55cd95b1bc8534cdb19e84c1befc5e2ee
roman alekseev <roma.alekseev.90@bk.ru>
2024-10-30 04:53:33 +0300

    Добавлены новые рецепты с ингредиентами и шагами для улучшения поиска

--- src/components/RecipeDetails.js
+++ src/components/RecipeDetails.js
@@ -1,16 +1,29 @@
 // src/components/RecipeDetails.js
 import React from "react";
+import "./RecipeDetails.css";
 
 const RecipeDetails = ({ recipe }) => {
   if (!recipe) {
-    return <div>No recipe selected</div>;
+    return <div className="recipe-details">Выберите рецепт для просмотра</div>;
   }
 
   return (
-    <div>
-      <h2>{recipe.title}</h2>
-      <p>{recipe.description}</p>
-      <img src={recipe.image} alt={recipe.title} />
+    <div className="recipe-details">
+      <h2 className="recipe-title">{recipe.title}</h2>
+      <img className="recipe-image" src={recipe.image} alt={recipe.title} />
+      <p className="recipe-description">{recipe.description}</p>
+      <h3>Ингредиенты:</h3>
+      <ul className="ingredient-list">
+        {recipe.ingredients.map((ingredient, index) => (
+          <li key={index}>{ingredient}</li>
+        ))}
+      </ul>
+      <h3>Рецепт:</h3>
+      <ol className="recipe-steps">
+        {recipe.steps.map((step, index) => (
+          <li key={index}>{step}</li>
+        ))}
+      </ol>
     </div>
   );
 };
